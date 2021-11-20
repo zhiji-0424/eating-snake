3d66ff9d6d78c0a7b08d6e948bfcd1cfb090f39e
tyri_zhiji <zhiji_0424@qq.com>
2021-11-20 11:03:50 +0800

    再次纠正LICENSE

--- src/locals.h
+++ src/locals.h
@@ -1,10 +1,10 @@
 /* Copyright (c) [2021] [tyri_ji]
  * [eating-snake] is licensed under Mulan PSL v2.
- * You can use this software according to the terms and co>
+ * You can use this software according to the terms and conditions of the Mulan PSL v2. 
  * You may obtain a copy of Mulan PSL v2 at:
- *         http://license.coscl.org.cn/MulanPSL2
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT >
- * See the Mulan PSL v2 for more details.
+ *        http://license.coscl.org.cn/MulanPSL2 
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.  
+ * See the Mulan PSL v2 for more details. * See the Mulan PSL v2 for more details.
  */
 
 #ifndef LOCALS_H
